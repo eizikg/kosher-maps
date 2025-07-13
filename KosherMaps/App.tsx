be4b73236129172416bb62c1105c87e561159203
Eizik Gottesfeld <ezkmonsey@gmail.com>
2025-07-13 17:39:31 -0400

    remove flipper and fix app

--- KosherMaps/App.tsx
+++ KosherMaps/App.tsx
@@ -8,11 +8,6 @@ import React, { useEffect } from 'react';
 import { NavigationProvider, TaskRemovedBehavior } from '@googlemaps/react-native-navigation-sdk';
 import { MapScreen } from './src/screens/MapScreen';
 
-// Flipper setup for debugging
-if (__DEV__) {
-  require('react-native-flipper').default();
-}
-
 const App: React.FC = () => {
   const termsAndConditionsOptions = {
     title: 'Terms and Conditions',
